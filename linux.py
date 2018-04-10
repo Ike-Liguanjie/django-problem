a4283cb1e72e678fd68f280cc50f7fd03e0b1803
li8954456 <34762823+li8954456@users.noreply.github.com>
2018-04-10 10:38:58 +0800

    linux_add
    Linux下的问题

---
+++ linux.py
@@ -0,0 +1,14 @@
+﻿# -*- coding: utf-8 -*-
+
+'''
+linux下的一些基本命令
+
+1.安装编译工具以及库文件
+yum -y install make zlib zlib-devel gcc-c++ libtool  openssl openssl-devel
+
+2.生成安装文件并制定目录
+./configure --prefix=目录
+
+3.添加白名单到防火墙
+-A INPUT -p tcp -m state -- state NEW -m tcp --dport 80 -j ACCEPT
+'''
